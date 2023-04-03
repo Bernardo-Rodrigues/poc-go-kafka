3bc2cfdc16000f74d09e9fd78951d770df28b6a0
Bernardo-Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2023-04-03 13:26:38 -0300

    feat: add project

---
+++ internal/infra/repository/product_repository_mysql.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"database/sql"
+	"test/internal/entity"
+)
+
+type ProductRepositoryMySql struct {
+	DB *sql.DB
+}
+
+func NewProductRepositoryMySql(db *sql.DB) *ProductRepositoryMySql {
+	return &ProductRepositoryMySql{DB: db}
+}
+
+func (repository *ProductRepositoryMySql) Create(product *entity.Product) error {
+	_, err := repository.DB.Exec("Insert into product (id, name, price) values(?,?,?)",
+		product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repository *ProductRepositoryMySql) FindAll() ([]*entity.Product, error) {
+	rows, err := repository.DB.Query("Select * from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+
+	for rows.Next() {
+		var product entity.Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+
+	return products, nil
+}
